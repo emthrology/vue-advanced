c5c67d22fab2ca457d9387662a02d6c38dc5e7b2
dongmin kim <emthrology@gmail.com>
2020-09-14 00:09:03 +0900

    itemView Css 입히기

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
-    <p>{{fetchAskItem.title}}</p>
-    <p>{{fetchAskItem.user}}</p>
-    <p>{{fetchAskItem.time_ago}}</p>
-    <div>{{fetchAskItem.content}}</div>
+    <section>
+      <!-- 질문 상세 정보 -->
+      <div class="user-container"> 
+        <div>
+          <!-- 사용자 프로필 -->
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+          <!-- 사용자 정보 -->
+          <router-link :to="`/user/${fetchAskItem.user}`">{{fetchAskItem.user}}</router-link>
+          <div class="time">{{fetchAskItem.time_ago}}</div>
+        </div>        
+      </div>
+      <h2>{{fetchAskItem.title}}</h2>
+    </section>
+    <section>
+      <!-- 질문 및 댓글 -->
+      <!-- v-html : stirng 내 html 태그 및 특수문자 파싱 (xss 주의) -->
+      <div v-html="fetchAskItem.content">
+        <!-- {{fetchAskItem.content}} -->
+      </div>     
+    </section>
   </div>
 </template>
 
@@ -22,6 +40,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .user-container {
+    display: flex;
+    align-items:center;
+    padding: 0.5rem;
+  }
+  .user-description {
+    padding-left: 8px;
+  }
+  .fa-user {
+    font-size: 2.5rem;
+  }
+  .time {
+    font-size: 0.7rem;
+  }
 </style>
