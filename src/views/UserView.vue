0f03cb75ae7e3a21b77dc7b3c961a532f86ac5c8
dongmin kim <emthrology@gmail.com>
2021-12-08 14:14:21 +0900

    slot 코멘터리 추가

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -2,8 +2,10 @@
   <div>
     <user-profile :propInfo="fetchedUser">
       <!-- 
-        div는 블럭요소작용, template은 아마도 테그없는 plain text 인듯 하다
-        =>template 을 span으로 바꿈 
+        slot
+        하위 컴포넌트를 부르는 템플릿 상에서 하위 컴포넌트의 구조를 생각할
+        필요 없이(블록,인라인정도만 구분?) slot 요소 붙여서 어떤 슬롯에
+        들어가는지 매칭만 해주면 됨
        -->
       <div slot="username">{{ fetchedUser.id }}</div>
       <span slot="time">{{ `Joined ${fetchedUser.created}` }}, </span>
